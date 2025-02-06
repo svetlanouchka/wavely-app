42deac40b9b86dc4e75d4279740fc4ca5c87f38b
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2025-02-06 14:00:14 +0100

    added button main

---
+++ frontend/src/ui/ButtonMain.jsx
@@ -0,0 +1,11 @@
+export default function ButtonMain ({idButton, text, style = {}, onClick}) {
+    return (
+        <button 
+        id={idButton}
+        style={style} 
+        onClick={onClick}
+        className="px-4 py-2 font-semibold rounded-lg transition-all">
+            {text}
+        </button>
+    );
+}
