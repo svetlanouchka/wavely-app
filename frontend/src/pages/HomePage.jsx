19c42a850f718196a15cd3982dc9e6b199e7f9c3
mguittari <mattias.guittari@gmail.com>
2025-02-06 10:31:22 +0100

    modified something

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Frequence from "../components/Seance";
 import AnimatedBackground from "@components/BackgroundCanvas";
 
 export default function HomePage() {
-	const animationClassName = "animated-background-session";
+	const animationClassName = "animated-background-home";
 	return (
 		<>
 			<Frequence />
