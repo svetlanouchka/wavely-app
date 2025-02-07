5165dae71b076688a95e6abcfcfb08ff51863fba
mguittari <mattias.guittari@gmail.com>
2025-02-07 11:41:28 +0100

    Dynamic header | new file logo W | homepage with animation and own header

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,11 +1,10 @@
-import logo from '../../assets/Logo_W.png';
-import { Link } from "react-router-dom";
+import logo from "../../assets/Logo_W.png";
 
 export default function Footer() {
-    return (
-        <footer className="bg-green-header h-16 text-white text-center p-4 flex justify-center items-center">
-            &copy; 2025 - Wavely
-            <img src={logo} alt="logo" className="h-12 w-12" />
-        </footer>
-    );
+	return (
+		<footer className="bg-green-header h-16 text-white text-center p-4 flex justify-center items-center">
+			&copy; 2025 - Wavely
+			<img src={logo} alt="logo" className="h-12 w-12" />
+		</footer>
+	);
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,22 @@
-import Frequence from "../components/Seance";
-import Frequency from "@components/Frequency";
+import Header from "../components/Layout/Header";
 import AnimatedBackground from "@components/BackgroundCanvas";
 
 export default function HomePage() {
 	const animationClassName = "animated-background-home";
 	return (
-		<>
-    			<AnimatedBackground animationClassName={animationClassName}/>
-		</>
+		<div className="flex flex-row items-end justify-center h-screen relative">
+			<div className="absolute top-[50%] translate-y-[-50%]">
+				<h1 className="font-righteous text-7xl md:text-9xl">Wavely</h1>
+				<h2 className="font-albert-sans text-md md:text-2xl font-medium text-center mt-1.5">
+					Partager des bonnes ondes
+				</h2>
+			</div>
+			<Header
+				backgroundColor="bg-gray-light"
+				string="Les Fréquences"
+				path="/frequencies"
+			/>
+			<AnimatedBackground animationClassName={animationClassName} />
+		</div>
 	);
 }
