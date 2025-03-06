1f93f5694e0c770a0ea92b69d401259ed3273f2e
Svetlana Preux-Nigay <sveta.nigai@gmail.com>
2025-03-06 16:08:44 +0100

    copyright

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -2,9 +2,9 @@ import logo from "../../assets/Logo_W.png";
 
 export default function Footer() {
 	return (
-		<footer className="bg-green-header h-16 text-white text-center p-4 flex justify-center items-center">
-			&copy; 2025 - Wavely
-			<img src={logo} alt="logo" className="h-12 w-12" />
+		<footer className="bg-green-header h-16 text-white text-center p-4 justify-center items-center">
+			<p className="text-sm text-gray-800"> © 2025 – Application web Wavely réalisée dans le cadre de la formation en développement web par ses élèves. La créatrice des fréquences est Yulia Taubin.</p>
+			<img src={logo} alt="logo" className="h-5 w-5 mx-auto" />
 		</footer>
 	);
 }
